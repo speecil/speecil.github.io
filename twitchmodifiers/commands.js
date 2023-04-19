8a36b34700108167d49c015fed19324330cf634f
Riley <90689870+speecil@users.noreply.github.com>
2023-04-19 23:29:31 +1000

    Create commands.js

---
+++ twitchmodifiers/commands.js
@@ -0,0 +1,17 @@
+function copyToClipboard(text) {
+    const textarea = document.createElement('textarea');
+    textarea.value = text;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textarea);
+  
+    const notification = document.getElementById('notification');
+    notification.innerText = 'Copied to clipboard!';
+    notification.classList.add('show');
+  
+    setTimeout(() => {
+      notification.classList.remove('show');
+    }, 2000);
+  }
+  
